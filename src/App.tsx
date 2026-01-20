7180803b7cbcb97cb1c5254554b569f121a4317b
Thomas Woroniak <thomasworoniak@Thomass-MacBook-Air.local>
2026-01-19 19:21:14 -0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import UserSearch from "./components/UserSearch";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>GitHub Finder</h1>
+      <UserSearch />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/api/github.ts
@@ -0,0 +1,23 @@
+export const fetchGithubUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/users/${username}`,
+  );
+
+  if (!res.ok) throw new Error("User not found");
+
+  const data = await res.json();
+  //   console.log(data);
+  return data;
+};
+
+export const searchGithubUser = async (query: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/search/users?q=${query}`,
+  );
+
+  if (!res.ok) throw new Error("User not found");
+
+  const data = await res.json();
+  //   console.log(data);
+  return data.items;
+};
