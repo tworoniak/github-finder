34af33ffa3838c8c72c6e45576c5c5aaa6b3c488
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-17 20:50:25 -0600

    update variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import UserSearch from "./components/UserSearch";
+import { Toaster } from "sonner";
 
 const App = () => {
   return (
     <div className="container">
       <h1>GitHub Finder</h1>
       <UserSearch />
+      <Toaster richColors />
     </div>
   );
 };
--- src/api/github.ts
+++ src/api/github.ts
@@ -21,3 +21,70 @@ export const searchGithubUser = async (query: string) => {
   //   console.log(data);
   return data.items;
 };
+
+// Check if following a user on Github
+export const checkIfFollowingUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+
+        Accept: "application/vnd.github+json",
+      },
+    },
+  );
+
+  if (res.status === 204) {
+    return true; //Following
+  } else if (res.status === 404) {
+    return false; // Not following
+  } else {
+    const errorData = await res.json().catch(() => null);
+    throw new Error(errorData.message || "Failed to check follow status");
+  }
+};
+
+// Follow user on Github
+export const followGithubUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+
+        Accept: "application/vnd.github+json",
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.message || "failed to follow user");
+  }
+
+  return true;
+};
+
+// Unfollow user on Github
+export const unfollowGithubUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+
+        Accept: "application/vnd.github+json",
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.message || "failed to unfollow user");
+  }
+
+  return true;
+};
